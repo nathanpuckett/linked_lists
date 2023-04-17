bf3e451fe68e2a8691f0e373595c5fad75754a51
nathanpuckett <nathanfpuckett@gmail.com>
2023-04-17 15:50:37 -0500

    Add prepend & append methods

--- main.rb
+++ main.rb
@@ -2,17 +2,39 @@
 
 # Represents the full list
 class LinkedList
-  attr_accessor :name
+  attr_accessor :name, :head, :tail
 
   def initialize
     @head = nil
     @tail = nil
   end
+
+  # Adds to beginning of list
+  def prepend(value)
+    if @head.nil?
+      @head = value
+      @tail = value
+    else
+      value.next_node = @head
+      @head = value
+    end
+  end
+
+  # Adds to end of list
+  def append(value)
+    if @head.nil?
+      @head = value
+      @tail = value
+    else
+      @tail.next_node = value
+      @tail = value
+    end
+  end
 end
 
 # Node Class
 class Node
-  attr_accessor :next, :data
+  attr_accessor :next_node, :value
 
   def initialize(value)
     @next_node = nil
