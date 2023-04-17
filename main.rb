96375c5d29a4b355140acbca9b7dea18b8a89a03
nathanpuckett <nathanfpuckett@gmail.com>
2023-04-17 16:06:53 -0500

    Add #size method

--- main.rb
+++ main.rb
@@ -2,7 +2,8 @@
 
 # Represents the full list
 class LinkedList
-  attr_accessor :name, :head, :tail
+  attr_accessor :name
+  attr_reader :head, :tail
 
   def initialize
     @head = nil
@@ -24,11 +25,22 @@ class LinkedList
   def append(value)
     if @head.nil?
       @head = value
-      @tail = value
     else
       @tail.next_node = value
-      @tail = value
     end
+    @tail = value
+  end
+
+  def size
+    return 0 if @head.nil?
+
+    total = 0
+    node = @head
+    until node.nil?
+      total += 1
+      node = node.next_node
+    end
+    total
   end
 end
 
@@ -41,3 +53,13 @@ class Node
     @value = value
   end
 end
+
+list = LinkedList.new
+
+list.append(Node.new(1))
+list.append(Node.new(2))
+list.prepend(Node.new(0))
+
+p list.size
+p list.head
+p list.tail
